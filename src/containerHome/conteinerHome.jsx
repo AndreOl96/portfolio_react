cd2b06d4b86e7bb19177c94b3a49f0b4dbd31b2e
AndreOl96 <150730550+AndreOl96@users.noreply.github.com>
2024-03-18 22:36:00 +0000

    feat card

--- src/containerHome/conteinerHome.jsx
+++ src/containerHome/conteinerHome.jsx
@@ -3,39 +3,66 @@ import "./conteinerHome.css";
 
 export default function ContainerHome() {
     return (
-        <div className="main-container">
-            <div className="sub-container"> <p></p>
 
-            <div className="parts-container">
-            <div className="card-home">
-                <div><img className="card-img-home" src="src/assets/1602706387191.jpeg" alt="Nome da imagem" /></div>
-            <div> <p>Nome 1</p>
-            <h3>André Olveira</h3>
-            <p>Some quick example text to build</p></div>
+        <div className="main-container">
+<div className="sub-container 1">
+    <div className="parts-container">
+        <img className="card-img-home" src="src/assets/1602706387191.jpeg" alt="Nome da imagem" />
+        <h3>André Olveira</h3>
+        <p>BOX 1</p>
+        <button className="card-button"> Contact Me </button>
+    </div>
+    <div className="parts-container">
+    <img className="card-img-home img-rectangle" src="src/assets/1602706387191.jpeg" alt="Nome da imagem" />
+        <p>BOX 2</p></div>
+    <div className="parts-container">
+    <img className="card-img-home" src="src/assets/1602706387191.jpeg" alt="Nome da imagem" />
+        <p>BOX 3</p></div>
+</div>
+<div className="sub-container 2">
+    <div className="parts-container"><p>BOX 1</p></div>
+    <div className="parts-container"><p>BOX 2</p></div>
+    <div className="parts-container"><p>BOX 3</p></div>
+</div>
+<div className="sub-container 3">
+    <div className="parts-container"><p>BOX 1</p></div>
+    <div className="parts-container"><p>BOX 2</p></div>
+    <div className="parts-container"><p>BOX 3</p></div>
+</div>
+
+
+        </div>
+
+        // <div className="main-container">
+        //     <div className="sub-container"> <p></p>
+
+        //     <div className="parts-container">
+        //     <div className="card-home">
+        //         <div><img className="card-img-home" src="src/assets/1602706387191.jpeg" alt="Nome da imagem" /></div>
+        //     <div> <p>Nome 1</p>
+        //     <h3>André Olveira</h3>
+        //     <p>Some quick example text to build</p></div>
            
-            </div>
-            </div>
-            <div className="parts-container">
-                <div className="card-home" ><p>linha 1</p></div>
-                <div>
-                <div className="card-home" > <p>Nome 1</p>
-            <h3>André Olveira</h3>
-            <p>Some quick example text to build</p></div>
-            <div className="card-home"> <p>Nome 1</p>
-            <h3>André Olveira</h3>
-            <p>Some quick example text to build</p></div>
+        //     </div>
+        //     </div>
+            
+        //         <div className="card-home square-box" ><p>MORE ABOUT ME</p>
+        //         <img className="card-img-home" src="src/assets/1602706387191.jpeg" alt="Nome da imagem" />
+        //         </div>
+        //         <div>
+        //         <div className="card-home square-box" > 
+        //         <p>Nome 1</p>
+        //     <p>Some quick example text to build</p></div>
+       
       
-                </div>
-            
-            </div>
+        //         </div>
             
             
             
-            </div>
-            <div className="sub-container"><p>linha 2</p></div>
-            <div className="sub-container"><p>linha 3</p></div>
+            
+        //     </div>
                
-            </div>
+        //     </div>
     );
 }
 
